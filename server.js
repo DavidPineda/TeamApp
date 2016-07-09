e5ab22ebeaa4015b40858de0760285ad833455b6
david pineda <davidpineda1003@gmail.com>
2016-07-09 18:37:35 -0500

    Modulacion 2

--- server.js
+++ server.js
@@ -2,29 +2,13 @@ var express = require('express');
 app = express();
 server = require('http').createServer(app);
 
-var swig = require('swig');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+var config = {
+    rootPath: __dirname
+};
 
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-app.set('view cache', false);
-swig.setDefaults({cache: false});
-app.use(logger('dev'));
-app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
-
-app.get('/partials/*', function(req, res){
-  console.log(req.params);
-  res.render('../..public/app/' + req.params['0']);
-});
-
-app.get('*', function(req, res){
-  res.render('index');
-})
+require('./server/config/express')(app, config);
+require('./server/config/routes')(app);
 
 server.listen(3000, function(){
-  console.log('Servidor Corriendo en el Puerto 3000');
+  console.log("Servidor Corriendo en el Puerto 3000");
 });
